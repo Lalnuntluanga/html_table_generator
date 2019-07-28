8b79cb5eda05c590e094b39b74ca3d3bcfe80380
tlaunga34 <tluanga34@gmail.com>
2019-07-28 15:29:51 +0530

    Add logic for table

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module("app",[]);
+window.app = window.app || angular.module("app",[]);
 
 app.controller("generator",function($scope, $timeout){
 	
@@ -19,116 +19,109 @@ app.controller("generator",function($scope, $timeout){
 	$scope.copyValue = function(id)
 	{
 		var copyTextarea = document.getElementById(id);
-		
 		if(copyTextarea.value == ''){
 			console.log("URL box is blank","warning");
 			return;
 		}
-		
 		copyTextarea.select();
 		try	{
 			var successful = document.execCommand('copy');
-			
 			if(successful == true){
 				$scope.showCopied = true;
-				
 				$timeout(function(){
 					$scope.showCopied = false;
 				}, 2000);
 			} else if(successful == false) {
 				console.log("Failed","error");
 			}
-		
 		} catch (err)	{
 			console.log('Oops, unable to copy','error');
 		}
-		
 	}
 	
 	$scope.preview = function(){
-		
 		var data = "<!DOCTYPE html>\n<html>\n<body>\n\n"+$scope.output +"\n\n</body>\n</html>";
 		var myWindow = window.open('_blank');
 		myWindow.document.write(data);
 		myWindow.focus();
 		myWindow.document.close();
-		
 	}
 	
+
+
 	$scope.parseTable = function(){
-		
 		var inputString = $scope.inputString,
 			row = '';
-		
 		row = inputString.split("\n");
-		
 		row.forEach(function(key, index){
-			
 			row[index] = key.split("\t");
-			
 			row[index].forEach(function(keyY, indexY){
-				
 				row[index][indexY] = {item : keyY};
-				
 			});
-			
 		});
-		
 		$scope.data = row;
-		
 	}
-	
-	
-	//GENERATE TEXT STRING REPRESENTING HTML TAGS
-	$scope.generate = function(e){
-		var cell_color,
-			headData = $scope.data[0],
-			bodyData = $scope.data.slice(),
-			headCells = '',
-			bodyCells = '';
-			
-		bodyData.splice(0,1);
-		
-		headData.forEach(function(key){
-			headCells += "\n\t\t\t<td style='padding:3px 10px; border:1px solid #444'>"+key.item+"</td>";
-		});
-			
-		$scope.output = "<table cellpadding='0' cellspacing='0' style='margin:0px auto; min-width:700px; font-family:arial; border-collapse:collapse'>\n\t<thead>"+
-			"\n\t\t<tr style='background-color:#FF6600'>"+headCells+
-			"\n\t\t</tr>"+
-			"\n\t</thead>\n\t<tbody>";
-		
-		bodyData.forEach(function(key,index){
-			
-			if(index % 2 == 0)
-				cell_color = "#FDE9D9";
-			else
-				cell_color = "#FCD5B4";
-			
-			bodyCells = ''
-			key.forEach(function(cells){
-				bodyCells += "\n\t\t\t<td style='padding:5px 10px; border:1px solid #444'>"+cells.item+"</td>";
-			});
-			
-			//console.log(bodyCells);
-			
-			$scope.output += "\n\t\t<tr style='background-color:"+cell_color+"'>"+bodyCells+"\n\t\t</tr>";
-		});
-		
-		
-		
-		$scope.output += "\n\t</tbody>\n</table>";		
-		$scope.open_output = true;
-	}
+
 	
 
 	$scope.generateJson = function(e){
-
 		console.log("$scope.data")
 		console.log($scope.data)
-
-		$scope.output  = JSON.stringify($scope.data);		
+		var userHash = findMissingPersons(createHash($scope.data));
+		$scope.output  = JSON.stringify(userHash);		
 		$scope.open_output = true;
+	}
+
+	function createHash(users) {
+		var userHash = {};
+		users.forEach(function (userRow) {
+			if (!userHash[userRow[0].item]) {
+				userHash[userRow[0].item] = {
+					presents: [],
+					absents: [],
+				}
+			}
+			userHash[userRow[0].item].presents.push(userRow[1].item);
+		});
+		return userHash;
+	}
+
+
+
+	function feedPersonsAutoFill(){
+		$scope.personString = app.persons.join("\n");
+	}
+	feedPersonsAutoFill();
+
+	$scope.parsePersonsTable = function (){
+		app.persons = $scope.personString.split("\n");
+		console.log("app.persons");
+		console.log(app.persons);
+	}
+
+
+	function findMissingPersons(userHash) {
+		var tempPresents;
+		var tempFoundFlag;
+		for (company in userHash) {
+			for (var i = 0; i < app.persons.length; i++) {
+				tempFoundFlag = false;
+				tempPresents = JSON.parse(JSON.stringify(userHash[company]["presents"]));
+				for (var j = 0; j < tempPresents.length; j++) {
+					if (app.persons[i] == tempPresents[j]) {
+						tempFoundFlag = true;
+						j--;
+						tempPresents.splice(j, 1);
+						break;
+					}
+				}
+				if(tempFoundFlag == false) {
+					userHash[company]["absents"].push(app.persons[i]);
+				}
+			}
+		}
+	
+		return userHash;
 	}
 
 	
---
+++ persons.js
@@ -0,0 +1,16 @@
+window.app = window.app || angular.module("app",[]);
+
+
+
+
+//ADD PERSON LIST HERE TO AUTO FILL
+app.persons = [
+  "plutorasupport.td001@plutora.com",
+  "plutorasupport.td002@plutora.com",
+  "plutorasupport.td003@plutora.com",
+  "plutorasupport.td004@plutora.com",
+  "plutorasupport.td005@plutora.com",
+  "plutorasupport.td006@plutora.com",
+  "plutorasupport.td007@plutora.com",
+  "plutorasupport.td008@plutora.com"
+]
