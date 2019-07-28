7485c8a00ad01103c6e20e076b881d145f66e553
tlaunga34 <tluanga34@gmail.com>
2019-07-28 15:50:27 +0530

    mAKE COMMENTS

--- js/main.js
+++ js/main.js
@@ -1,77 +1,115 @@
-window.app = window.app || angular.module("app",[]);
+window.app = window.app || angular.module("app", []);
 
-app.controller("generator",function($scope, $timeout){
-	
+app.controller("generator", function ($scope, $timeout) {
+
 	$scope.data = [];
 	$scope.inputString = '';
 	$scope.output = '';
 	$scope.open_output = false;
 	$scope.showCopied = false;
-	$scope.stopPropagation = function(e){e.stopPropagation();}
-	
-	
-	$scope.remField = function(index){
+	$scope.stopPropagation = function (e) { e.stopPropagation(); }
+
+
+	$scope.remField = function (index) {
 		console.log(index);
-		$scope.data.splice(index,1);
+		$scope.data.splice(index, 1);
 	}
-	
+
 	//COPY VALUE OF INPUT FIELD BY CLICK ON COPY BUTTON
-	$scope.copyValue = function(id)
-	{
+	$scope.copyValue = function (id) {
 		var copyTextarea = document.getElementById(id);
-		if(copyTextarea.value == ''){
-			console.log("URL box is blank","warning");
+		if (copyTextarea.value == '') {
+			console.log("URL box is blank", "warning");
 			return;
 		}
 		copyTextarea.select();
-		try	{
+		try {
 			var successful = document.execCommand('copy');
-			if(successful == true){
+			if (successful == true) {
 				$scope.showCopied = true;
-				$timeout(function(){
+				$timeout(function () {
 					$scope.showCopied = false;
 				}, 2000);
-			} else if(successful == false) {
-				console.log("Failed","error");
+			} else if (successful == false) {
+				console.log("Failed", "error");
 			}
-		} catch (err)	{
-			console.log('Oops, unable to copy','error');
+		} catch (err) {
+			console.log('Oops, unable to copy', 'error');
 		}
 	}
-	
-	$scope.preview = function(){
-		var data = "<!DOCTYPE html>\n<html>\n<body>\n\n"+$scope.output +"\n\n</body>\n</html>";
+
+
+
+	$scope.preview = function () {
+		var data = $scope.output;
 		var myWindow = window.open('_blank');
 		myWindow.document.write(data);
 		myWindow.focus();
 		myWindow.document.close();
 	}
-	
 
 
-	$scope.parseTable = function(){
+	function feedPersonsAutoFill() {
+		$scope.personString = app.persons.join("\n");
+	}
+	feedPersonsAutoFill();
+
+	$scope.parsePersonsTable = function () {
+		app.persons = $scope.personString.split("\n");
+		console.log("app.persons");
+		console.log(app.persons);
+	}
+
+
+	$scope.parseUsersTable = function () {
 		var inputString = $scope.inputString,
 			row = '';
 		row = inputString.split("\n");
-		row.forEach(function(key, index){
+		row.forEach(function (key, index) {
 			row[index] = key.split("\t");
-			row[index].forEach(function(keyY, indexY){
-				row[index][indexY] = {item : keyY};
+			row[index].forEach(function (keyY, indexY) {
+				row[index][indexY] = { item: keyY };
 			});
 		});
 		$scope.data = row;
 	}
 
-	
 
-	$scope.generateJson = function(e){
-		console.log("$scope.data")
-		console.log($scope.data)
+
+	$scope.generateJson = function (e) {
 		var userHash = findMissingPersons(createHash($scope.data));
-		$scope.output  = JSON.stringify(userHash);		
+		$scope.output = JSON.stringify(userHash);
 		$scope.open_output = true;
 	}
 
+
+
+
+
+
+	/*
+		CREATE HASH OF ARRAY OF COMPANIES IN TO ONE HASH
+		EG.
+		From
+			[
+				'amp' : 'plutorasupport1@plutora.com',
+				'amp' : 'plutorasupport2@plutora.com',
+				'amp' : 'plutorasupport3@plutora.com',
+			]
+	
+		Into 
+		{
+			amp : {
+				presents : [
+					'plutorasupport1@plutora.com',
+					'plutorasupport2@plutora.com',
+					'plutorasupport3@plutora.com'
+				],
+				absents : []
+			}
+		}
+			
+	*/
 	function createHash(users) {
 		var userHash = {};
 		users.forEach(function (userRow) {
@@ -87,42 +125,54 @@ app.controller("generator",function($scope, $timeout){
 	}
 
 
-
-	function feedPersonsAutoFill(){
-		$scope.personString = app.persons.join("\n");
-	}
-	feedPersonsAutoFill();
-
-	$scope.parsePersonsTable = function (){
-		app.persons = $scope.personString.split("\n");
-		console.log("app.persons");
-		console.log(app.persons);
-	}
-
-
+	/*
+		Fill the missing support personels.
+		eg.
+		From 
+		{
+			amp : {
+				presents : [
+					'plutorasupport1@plutora.com',
+					'plutorasupport2@plutora.com',
+					'plutorasupport3@plutora.com'
+				],
+				absents : []
+			}
+		}
+	
+		to 
+		{
+			amp : {
+				presents : [
+					'plutorasupport1@plutora.com',
+					'plutorasupport2@plutora.com',
+					'plutorasupport3@plutora.com'
+				],
+				absents : [
+					'plutorasupport4@plutora.com',
+					'plutorasupport5@plutora.com',
+					'plutorasupport6@plutora.com'
+					'plutorasupport7@plutora.com'
+					'plutorasupport8@plutora.com'
+				]
+			}
+		}
+	
+	*/
 	function findMissingPersons(userHash) {
-		var tempPresents;
-		var tempFoundFlag;
+		//LOOP THROUGH ALL COMPANIES
 		for (company in userHash) {
+			//LOOP THROUGH ALL EXPECTED PERSONS
 			for (var i = 0; i < app.persons.length; i++) {
-				tempFoundFlag = false;
-				tempPresents = JSON.parse(JSON.stringify(userHash[company]["presents"]));
-				for (var j = 0; j < tempPresents.length; j++) {
-					if (app.persons[i] == tempPresents[j]) {
-						tempFoundFlag = true;
-						j--;
-						tempPresents.splice(j, 1);
-						break;
-					}
-				}
-				if(tempFoundFlag == false) {
+				//IF PERSON[I] ABSENTS IN THE PERSENTS LIST, PUSH IT INTO ABSENTEES ARRAY.
+				if (userHash[company]["presents"].indexOf(app.persons[i]) == -1) {
 					userHash[company]["absents"].push(app.persons[i]);
 				}
 			}
 		}
-	
+
 		return userHash;
 	}
 
-	
+
 });
